157a526730b6c3a5b72e44f2b2209b21ea6afb31
barrosflavio <flavi011@outlook.com>
2024-05-24 17:01:07 -0300

    Hello world and simple vartypes

---
+++ vars.rb
@@ -0,0 +1,21 @@
+var1 = "Ruby"
+var2 = 1
+var3 = -2
+var4 = 1.0
+var5 = -5.0
+var6 = true
+var7 = false
+var8 = ["Hello", "World", 1, true]
+
+puts var1.class
+puts var2.class
+puts var3.class
+puts var4.class
+puts var5.class
+puts var6.class
+puts var7.class
+puts var8.class
+puts var8[0].class
+puts var8[1].class
+puts var8[2].class
+puts var8[3].class
