4ee918e09c4a2219454f4cf1c63f69b5e6160004
barrosflavio <flavi011@outlook.com>
2024-05-25 08:49:36 -0300

    All vartypes

--- vars.rb
+++ vars.rb
@@ -6,6 +6,9 @@ var5 = -5.0
 var6 = true
 var7 = false
 var8 = ["Hello", "World", 1, true]
+var9 = :symboltest
+var10 = :trin
+var11 = {nome: "flavio", idade: 23, altura: 1.80}
 
 puts var1.class
 puts var2.class
@@ -19,3 +22,10 @@ puts var8[0].class
 puts var8[1].class
 puts var8[2].class
 puts var8[3].class
+puts var9.class
+puts var9.object_id
+puts var10.object_id
+puts var11.class
+puts var11[:nome]
+puts var11[:altura]
+puts var11[:idade]
