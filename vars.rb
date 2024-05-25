7d7f889a39086eaf727b60a162980f2df744ae5e
barrosflavio <flavi011@outlook.com>
2024-05-25 09:38:54 -0300

    end start

--- vars.rb
+++ vars.rb
@@ -1,15 +1,23 @@
 var1 = "Ruby"
+#integer
 var2 = 1
 var3 = -2
+#float
 var4 = 1.0
 var5 = -5.0
+#bool trueclass
 var6 = true
+#bool falseclass
 var7 = false
+#array
 var8 = ["Hello", "World", 1, true]
+#symbol
 var9 = :symboltest
 var10 = :trin
-var11 = {nome: "flavio", idade: 23, altura: 1.80}
+#hash
+var11 = {nome: "flavio", sobrenome: "BARROS", idade: 23, altura: 1.80}
 
+#output types of variables
 puts var1.class
 puts var2.class
 puts var3.class
@@ -29,3 +37,13 @@ puts var11.class
 puts var11[:nome]
 puts var11[:altura]
 puts var11[:idade]
+
+#string methods
+puts var11[:nome].upcase
+puts var11[:sobrenome].downcase
+puts var11[:nome].reverse.upcase
+
+puts var11[:nome] + var11[:sobrenome]
+
+# string interpolation #{}
+puts "#{var11[:nome]} tem #{var11[:idade]} anos"
